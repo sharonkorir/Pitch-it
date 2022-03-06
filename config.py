9505a5ba0493aa5ec51e5d7f5f430b8ad375e3a8
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-06 22:25:52 +0300

    add: start shell file to gitignore

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sharon:12345678@localhost/pitchapp'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SENDER_EMAIL = 'sharon.moringaprojects@gmail.com'
     
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='notsosecretkey'
+export MAIL_USERNAME='sharon.moringaprojects@gmail.com'
+export MAIL_PASSWORD='Tenwek03'
 
-python3.8 manage.py server
+python3 manage.py server

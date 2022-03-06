f47b009d939be17051544665a3127a32c7884626
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-06 14:54:18 +0300

    create: user tests for User model

--- manage.py
+++ manage.py
@@ -11,6 +11,14 @@ manager.add_command('server', Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
+#run unittests
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 #create shell context
 @manager.shell
 def make_shell_context():

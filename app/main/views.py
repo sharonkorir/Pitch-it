8da35a4dadef02757bb65d0d3c3fec7789e15b2a
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-08 13:13:47 +0300

    fix display pitches macros

--- app/main/views.py
+++ app/main/views.py
@@ -12,9 +12,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
+    general = Pitch.query.filter_by(category = 'general').all()
+    business = Pitch.query.filter_by(category = 'business/tech strt-up').all()
+    books = Pitch.query.filter_by(category = 'books').all()
+    pick_up = Pitch.query.filter_by(category = 'pick-up lines').all()
+    dad_jokes = Pitch.query.filter_by(category = 'dad jokes').all()
     title = 'Home - Pitch it in sixty'
-    return render_template('index.html')
+    return render_template('index.html', title = title, general = general, business = business, books = books, pick_up = pick_up, dad_jokes = dad_jokes)
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -59,15 +63,14 @@ def update_pic(uname):
 @login_required
 def new_pitch():
     form = PitchForm()
-
+  
     if form.validate_on_submit():
         title = form.title.data
         content = form.content.data
         category = form.category.data
         user_id = current_user
-        new_pitch = Pitch(title, content, category, user_id)
+        new_pitch = Pitch(title=title, content=content, category=category, user_id=current_user._get_current_object().id)
         new_pitch.save_pitch()
         return redirect(url_for('main.index'))
 
-    title = f'{new_pitch.title}'
-    return render_template('new_pitch.html',title = title, pitch_form=form)
+    return render_template('new_pitch.html', pitch_form=form)

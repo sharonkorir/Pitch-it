fd3ba6109551204c4c37dc48b64c1829f8136495
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-08 11:03:55 +0300

    create pitch submission form

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,10 @@ from wtforms.validators import DataRequired
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about yourself.',validators = [DataRequired()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[DataRequired()])
+    category = TextAreaField('Select category', validators=[DataRequired()])
+    content = TextAreaField('Your pitch', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
-from ..models import User
-from .forms import UpdateProfile
+from ..models import User, Pitch
+from .forms import UpdateProfile, PitchForm
 from .. import db, photos
 from flask_login import login_required, current_user
 
@@ -54,3 +54,20 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/create_pitch', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch = Pitch(title, content, category, user_id)
+        new_pitch.save_pitch()
+        return redirect(url_for('main.index'))
+
+    title = f'{new_pitch.title}'
+    return render_template('new_pitch.html',title = title, pitch_form=form)

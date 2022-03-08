f8eaefaa2efdb3e72f59f87d7050e28949b4eb47
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-08 22:44:59 +0300

    display user pitches in user profile

--- app/main/views.py
+++ app/main/views.py
@@ -25,11 +25,13 @@ def index():
 @main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
+    user_id = current_user._get_current_object().id
+    pitches = Pitch.query.filter_by(user_id = user_id).all()
 
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user)
+    return render_template("profile/profile.html", user = user, pitches = pitches)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
